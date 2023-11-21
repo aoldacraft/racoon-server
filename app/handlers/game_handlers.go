4c3e22872081aef7a05887437818a46984b04356
KimTaeWook <soc07249@ajou.ac.kr>
2023-11-21 17:13:38 +0900

    [Feat] game state control

--- app/handlers/game_handlers.go
+++ app/handlers/game_handlers.go
@@ -84,12 +84,36 @@ func CreateGame(db *sql.DB) func(c echo.Context) error {
 			return c.JSON(http.StatusBadRequest, "Invalid request body")
 		}
 
+		// Start a database transaction
+		tx, err := db.Begin()
+		if err != nil {
+			log.Println(err)
+			return c.JSON(http.StatusInternalServerError, "Failed to start a transaction")
+		}
+		defer tx.Rollback() // Rollback the transaction in case of an error
+
 		// Insert the new game into the database
 		insertSQL := "INSERT INTO game (uuid, service_name, server_ip, player_quantity, game_state, state_time) VALUES ($1, $2, $3, $4, $5, $6)"
-		_, err := db.Exec(insertSQL, game.UUID, game.ServiceName, game.ServerIP, game.PlayerQuantity, game.GameState, game.StateTime)
+		_, err = tx.Exec(insertSQL, game.UUID, game.ServiceName, game.ServerIP, game.PlayerQuantity, game.GameState, game.StateTime)
 		if err != nil {
 			log.Println(err)
 			return c.JSON(http.StatusInternalServerError, "Failed to create the game")
+		}
+
+		// Update the service table if the game state is "ACTIVE" or "PENDING"
+		if game.GameState == "ACTIVE" || game.GameState == "PENDING" {
+			updateSQL := "UPDATE service SET server_quantity = server_quantity + 1, total_player = total_player + $1 WHERE service_name = $2"
+			_, err = tx.Exec(updateSQL, game.PlayerQuantity, game.ServiceName)
+			if err != nil {
+				log.Println(err)
+				return c.JSON(http.StatusInternalServerError, "Failed to update the service table")
+			}
+		}
+
+		// Commit the transaction
+		if err := tx.Commit(); err != nil {
+			log.Println(err)
+			return c.JSON(http.StatusInternalServerError, "Failed to commit the transaction")
 		}
 
 		return c.JSON(http.StatusCreated, "Game created successfully")
@@ -106,6 +130,14 @@ func UpdateGame(db *sql.DB) func(c echo.Context) error {
 		game := new(models.Game)
 		if err := c.Bind(game); err != nil {
 			return c.JSON(http.StatusBadRequest, "Invalid request body")
+		}
+
+		// Get the current game state before the update
+		currentState := ""
+		err := db.QueryRow("SELECT game_state FROM game WHERE uuid = $1", gameUUID).Scan(&currentState)
+		if err != nil {
+			log.Println(err)
+			return c.JSON(http.StatusInternalServerError, "Failed to get current game state")
 		}
 
 		// Construct the dynamic SQL query
@@ -139,10 +171,26 @@ func UpdateGame(db *sql.DB) func(c echo.Context) error {
 		args = append(args, gameUUID)
 
 		// Execute the dynamic SQL query
-		_, err := db.Exec(updateSQL, args...)
+		_, err = db.Exec(updateSQL, args...)
 		if err != nil {
 			log.Println(err)
 			return c.JSON(http.StatusInternalServerError, "Failed to update the game")
+		}
+
+		// Update the service table if the game state changes
+		if currentState != "ACTIVE" && currentState != "PENDING" && (game.GameState == "ACTIVE" || game.GameState == "PENDING") {
+			_, err := db.Exec("UPDATE service SET server_quantity = server_quantity + 1, total_player = total_player + $1 WHERE service_name = (SELECT service_name FROM game WHERE uuid = $2)", game.PlayerQuantity, gameUUID)
+			if err != nil {
+				log.Println(err)
+				return c.JSON(http.StatusInternalServerError, "Failed to update the service table")
+			}
+		} else if currentState == "ACTIVE" || currentState == "PENDING" {
+			// Game state changed from "ACTIVE" or "PENDING" to another state
+			_, err := db.Exec("UPDATE service SET server_quantity = server_quantity - 1, total_player = total_player - $1 WHERE service_name = (SELECT service_name FROM game WHERE uuid = $2)", game.PlayerQuantity, gameUUID)
+			if err != nil {
+				log.Println(err)
+				return c.JSON(http.StatusInternalServerError, "Failed to update the service table")
+			}
 		}
 
 		return c.JSON(http.StatusOK, "Game updated successfully")
